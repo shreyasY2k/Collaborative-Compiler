2f6d289e60f7bc28f7ce47dd9aeaf5016a3aa393
shreyasY2k <shreyasmk.mathur@gmail.com>
2022-04-19 17:53:48 +0530

    make it better

--- routes/project.js
+++ routes/project.js
@@ -41,7 +41,7 @@ const zl = require("zip-lib");
 const path = require("path");
 const AWS = require("aws-sdk");
 const fs = require("fs");
-const io = require("socket.io")(3001, {
+const io = require("socket.io")({
   cors: {
     origin: "https://my-collab-editor.herokuapp.com",
     methods: ["GET", "POST"]
