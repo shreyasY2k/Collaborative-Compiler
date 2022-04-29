427cd23ac6f595299410472196f28462914a4be7
shreyasY2k <shreyasmk.mathur@gmail.com>
2022-04-29 14:28:39 +0530

    re-designing socket connections v2

--- routes/project.js
+++ routes/project.js
@@ -42,6 +42,7 @@ const path = require("path");
 const AWS = require("aws-sdk");
 const fs = require("fs");
 const fetch = require("node-fetch");
+const { request } = require("http");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
@@ -70,7 +71,7 @@ initializePassport(passport);
 
 io.use((socket, next) => {
   if (socket.request.user) {
-    console.log(socket.request.user);
+    // console.log(socket.request.user);
     next();
   } else {
     next(new Error("unauthorized"));
@@ -129,11 +130,173 @@ router.get("/project/download", checkAuthenticated, (req, res) => {
     );
   });
 });
-io.on('connect', (socket) => {
-  console.log(`new connection ${socket.id}`);
-  socket.on('whoami', (cb) => {
-    cb(socket.request.user ? socket.request.user.name : '');
-  });})
+var usersSocket={
+  users:[],
+  projectPath:'',
+  userID:'',
+  socketID:''
+}
+io.on('connection', (socket) => {
+  //create room for project and get project name form socket query
+  // console.log("user connected");
+  const projectname = socket.handshake.query.projectname;
+  const userid = socket.request.user._id;
+  const projectPath = path.join(
+    __dirname,
+    "../",
+    userid.toString(),
+    projectname.toString()
+  );
+  // console.log(projectname);
+  socket.join(projectPath);
+  usersSocket.users.push(socket.id);
+  usersSocket.projectPath = projectPath;
+  usersSocket.userID = userid;
+  usersSocket.socketID = socket.id;
+  console.log(usersSocket);
+  // console.log(socket.rooms);
+  io.sockets.in(projectPath).emit('roomConnected', socket.request.user._id.toString() + "/" + projectname);
+
+socket.on("deleteFile", file => {
+  // console.log(projectPath);
+  // var pp = path.join(projectPath, file.fileName);
+  // console.log(pp);
+  if (
+    fs.existsSync(
+      path.join("625ea363d882d34d42aba3cd" + "/", "A", file.fileName)
+    )
+  ) {
+    fs.unlinkSync(
+      path.join("625ea363d882d34d42aba3cd" + "/", "A", file.fileName)
+    );
+    //emit to all users in room
+    io.sockets
+      .in(usersSocket.socketID)
+      .emit("deleteFile", file.projectName, file.fileName);
+    // socket.emit("deleteFile", file.projectName, file.fileName);
+  }
+});
+socket.on("join", (socketID) => {
+  socket.join(socketID);
+})
+//listen for addFile event
+socket.on("addFile", file => {
+  if (
+    !fs.existsSync(
+      path.join(
+        __dirname,
+        "../",
+        socket.request.user._id.toString(),
+        file.projectName,
+        file.fileName
+      )
+    )
+  ) {
+    fs.writeFileSync(
+      path.join(
+        __dirname,
+        "../",
+        socket.request.user._id.toString(),
+        file.projectName,
+        file.fileName
+      ),
+      file.fileContent
+    );
+    socket.emit("addFile", file.projectName, file.fileName);
+  }
+});
+
+//listen for renameFile event
+socket.on("renameFile", file => {
+  if (
+    fs.existsSync(
+      path.join(
+        __dirname,
+        "../",
+        socket.request.user._id.toString(),
+        file.projectName,
+        file.oldFileName
+      )
+    )
+  ) {
+    fs.renameSync(
+      path.join(
+        projectPath,
+        file.oldFileName
+      ),
+      path.join(
+       projectPath,
+        file.newFileName
+      )
+    );
+    socket
+      .emit("renameFile", file.projectName, file.oldFileName, file.newFileName);
+  }
+});
+
+socket.on("updateFile", file => {
+  fs.writeFileSync(
+    path.join(
+      projectPath,
+      file.fileName
+    ),
+    file.fileContent
+  );
+  socket
+    .emit("updateFile", file.projectName, file.fileName, file.fileContent);
+});
+
+socket.on("getFile", file => {
+  //get file content
+  const fileContent = fs.readFileSync(
+    path.join(
+      projectPath,
+      file.fileName
+    ),
+    "utf8"
+  );
+  socket.emit("fileContent", {
+      projectName: file.projectName,
+      fileName: file.fileName,
+      fileContent: fileContent
+    });
+});
+
+socket.on("autoSuggest", file => {
+  var question = file.lineContent;
+  //fetch request to codegrepper
+  fetch(
+    "https://www.codegrepper.com/api/search.php?q=" +
+      question +
+      "&search_options=search_titles"
+  )
+    .then(res => res.json())
+    .then(data => {
+      socket
+        .emit("autoSuggest", { data: data, lineNumber: file.lineNumber });
+    });
+});
+socket.on("compile", async file => {
+  await fetch("https://codeorbored.herokuapp.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain"
+    },
+    body: file.body
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+     socket
+        .emit("compileOutput", { output: data.output });
+    })
+    .catch(error => alert(error.message));
+});
+  socket.on('disconnect', () => {
+    socket.leave(socket.request.user._id.toString() + "/" + projectname);
+  });
+})
 router.get("/project/open", checkAuthenticated, async (req, res) => {
   const userId = req.user._id;
   const projectname = req.query.projectname;
@@ -146,90 +309,32 @@ router.get("/project/open", checkAuthenticated, async (req, res) => {
     .map(item => item.name);
 
   res.render("project/editor", {
-    userId: userId,
     files: fileList,
-    projectname: projectname
+    projectname: projectname,
+    socketID:""
   });
 });
 
-router.get("/project/getFile*", checkAuthenticated, (req, res) => {
-  const userid = req.user._id;
-  const projectname = req.query.projectname;
-  const file = req.query.filename;
-  fs.readFile(
-    path.join(__dirname, "../", userid.toString(), projectname, file),
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
+router.post("/project/joinRoom",(req,res)=>{
+  var projectPath = req.body.roomID;
+  //find usersSocket which has the same projectPath
+  console.log(usersSocket);
+  // var userSocket = usersSocket.values.find(
+  //   item => item.projectPath === projectPath
+  // );
+  // //get socket id from usersSocket
+  // console.log(userSocket);
+  var socketID = usersSocket.socketID;
+  //read files from projectPath
+  var fileList = fs
+    .readdirSync(path.join(__dirname, "../", projectPath), {
+    })
 
-router.get("/project/addFile*", checkAuthenticated, (req, res) => {
-  const userid = req.user._id;
-  const projectname = req.query.projectname;
-  const file = req.query.filename;
-  fs.writeFile(
-    path.join(__dirname, "../", userid.toString(), projectname, file),
-    "",
-    err => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("File created");
-      }
-    }
-  );
-});
-
-router.get("/project/updateFile*", checkAuthenticated, (req, res) => {
-  const userid = req.user._id;
-  const projectname = req.query.projectname;
-  const file = req.query.newfilename;
-  const oldfilename = req.query.oldfilename;
-
-  fs.rename(
-    path.join(
-      __dirname,
-      "../",
-      userid.toString(),
-      projectname.toString(),
-      oldfilename.toString()
-    ),
-    path.join(
-      __dirname,
-      "../",
-      userid.toString(),
-      projectname.toString(),
-      file.toString()
-    ),
-    err => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("File updated");
-      }
-    }
-  );
-});
-
-router.get("/project/deleteFile*", checkAuthenticated, (req, res) => {
-  const userid = req.user._id;
-  const projectname = req.query.projectname;
-  const file = req.query.filename;
-  fs.unlink(
-    path.join(__dirname, "../", userid.toString(), projectname, file),
-    err => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("File deleted");
-      }
-    }
-  );
-});
+  res.render("project/editor", {
+    files: fileList,
+    projectname: projectPath.split("/")[1],
+    socketID: socketID
+  });
+})
 
 module.exports = router;
